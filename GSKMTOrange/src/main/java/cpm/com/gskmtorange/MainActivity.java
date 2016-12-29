2c94c2fa1f206b43db74e3ea4be5ca0dd2a6b811
ashishandroid <ashish004.2009@gmail.com>
2016-12-29 10:12:23 +0530

    Geotag

--- GSKMTOrange/src/main/java/cpm/com/gskmtorange/MainActivity.java
+++ GSKMTOrange/src/main/java/cpm/com/gskmtorange/MainActivity.java
@@ -1,11 +1,10 @@
 package cpm.com.gskmtorange;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -21,6 +20,7 @@ import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import cpm.com.gskmtorange.GeoTag.GeoTagStoreList;
 import cpm.com.gskmtorange.constant.CommonString;
 
 public class MainActivity extends AppCompatActivity
@@ -137,14 +137,16 @@ public class MainActivity extends AppCompatActivity
         else if (id == R.id.nav_geotag) {
 
 
+            Intent startDownload = 	new Intent(this,GeoTagStoreList.class);
+            startActivity(startDownload);
 
-
-
+            overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
 
 
 
 
         }
+
         else if (id == R.id.nav_export) {
 
 
