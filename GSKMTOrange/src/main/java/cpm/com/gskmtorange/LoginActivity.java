3ab90e2e9ae0321879a5b058acbddaa0cb97a798
yadavendras <yadavendra@gmail.com>
2017-01-21 16:34:50 +0530

    T2P upload data newss

--- GSKMTOrange/src/main/java/cpm/com/gskmtorange/LoginActivity.java
+++ GSKMTOrange/src/main/java/cpm/com/gskmtorange/LoginActivity.java
@@ -137,9 +137,9 @@ public class LoginActivity extends AppCompatActivity {
         //populateAutoComplete();
         mPasswordView = (EditText) findViewById(R.id.password);
 
-        museridView.setText("testmer");
+       /* museridView.setText("testmer");
         mPasswordView.setText("cpm123");
-
+*/
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
